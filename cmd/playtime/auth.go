2224c982f7c36b39023507c2ba32e17d4e2c4d96
Stefan Röhrbein <stefan.roehrbein@kaufhof.de>
2019-03-26 11:40:30 +0100

    Retrieving access_token from api

--- cmd/playtime/auth.go
+++ cmd/playtime/auth.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
 
 func auth(config AuthConfig) {
 	fmt.Println(config.clientId)
+	client := &http.Client{}
+	data := url.Values{}
+	data.Add("grant_type", "client_credentials")
 
+	req, err := http.NewRequest(http.MethodPost, config.tokenUrl, strings.NewReader(data.Encode()))
+	if err != nil {
+		fmt.Println("Could not create request object: ", err)
+	}
+	req.SetBasicAuth(config.clientId, config.clientSecret)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Request to tokenurl failed: ", err)
+	}
+
+	bodyText, _ := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+	fmt.Println(s)
 }
